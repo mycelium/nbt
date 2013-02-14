9e1229ec10ae8246363c589706f79581bc5e643a
Shubin Konstantin <kshubin@itcwin.com>
2013-02-14 07:41:40 +0400

    Updated fields at CR and Issue entities

--- src/main/java/com/mycelium/nbt/model/entities/CrEntity.java
+++ src/main/java/com/mycelium/nbt/model/entities/CrEntity.java
@@ -6,14 +6,20 @@ public class CrEntity implements Entity {
 	private String _id;
 	private String _caption;
 	private String _author;
-	private ArrayList<String> _wathers;
+	private ArrayList<String> _watchers;
 	private ArrayList<String> _idTasks;
 	private String _parentId;
-	
-	public CrEntity(){
+
+	public CrEntity() {
 	}
-	public CrEntity(String caption){
+
+	public CrEntity(String caption, String author, ArrayList<String> watchers,
+			ArrayList<String> idTasks, String parentId) {
 		_caption = caption;
+		_author = author;
+		_watchers = watchers;
+		_idTasks = idTasks;
+		_parentId = parentId;
 	}
 
 	@Override
@@ -25,46 +31,45 @@ public class CrEntity implements Entity {
 	public void setId(String id) {
 		this._id = id;
 	}
-	
+
 	public String get_caption() {
 		return _caption;
 	}
+
 	public void set_caption(String _caption) {
 		this._caption = _caption;
 	}
-	
+
 	public String get_author() {
 		return _author;
 	}
-	
+
 	public void set_author(String _author) {
 		this._author = _author;
 	}
-	
+
 	public ArrayList<String> get_wathers() {
-		return _wathers;
+		return _watchers;
 	}
-	
+
 	public void set_wathers(ArrayList<String> _wathers) {
-		this._wathers = _wathers;
+		this._watchers = _wathers;
 	}
-	
+
 	public ArrayList<String> get_idTasks() {
 		return _idTasks;
 	}
-	
+
 	public void set_idTasks(ArrayList<String> _idTasks) {
 		this._idTasks = _idTasks;
 	}
-	
+
 	public String get_parentId() {
 		return _parentId;
 	}
-	
+
 	public void set_parentId(String _parentId) {
 		this._parentId = _parentId;
 	}
-	
-	
 
 }
--- src/main/java/com/mycelium/nbt/model/entities/IssueEntity.java
+++ src/main/java/com/mycelium/nbt/model/entities/IssueEntity.java
@@ -18,11 +18,29 @@ public class IssueEntity implements Entity {
 	private Date _modificationDate;
 	private String _environment;
 	private String _description;
-	
-	public IssueEntity(){
+
+	public IssueEntity() {
 	}
-	public IssueEntity(String caption){
+
+	public IssueEntity(String caption, String reporter,
+			ArrayList<String> assignees, ArrayList<String> watchers,
+			ArrayList<String> subtasks, ArrayList<String> components,
+			String typeIssue, String statusIssue, String priorityIssue,
+			Date creationDate, Date modificationDate, String environment,
+			String description) {
 		_caption = caption;
+		_reporter = reporter;
+		_assignees = assignees;
+		_watchers = watchers;
+		_subtasks = subtasks;
+		_components = components;
+		_typeIssue = typeIssue;
+		_statusIssue = statusIssue;
+		_priorityIssue = priorityIssue;
+		_creationDate = creationDate;
+		_modificationDate = modificationDate;
+		_environment = environment;
+		_description = description;
 	}
 
 	@Override
@@ -34,109 +52,109 @@ public class IssueEntity implements Entity {
 	public void setId(String id) {
 		this._id = id;
 	}
-	
+
 	public String get_caption() {
 		return _caption;
 	}
+
 	public void set_caption(String _caption) {
 		this._caption = _caption;
 	}
-	
+
 	public String get_reporter() {
 		return _reporter;
 	}
-	
+
 	public void set_reporter(String _reporter) {
 		this._reporter = _reporter;
 	}
-	
+
 	public ArrayList<String> get_assignees() {
 		return _assignees;
 	}
-	
+
 	public void set_assignees(ArrayList<String> _assignees) {
 		this._assignees = _assignees;
 	}
-	
+
 	public ArrayList<String> get_watchers() {
 		return _watchers;
 	}
-	
+
 	public void set_watchers(ArrayList<String> _watchers) {
 		this._watchers = _watchers;
 	}
-	
+
 	public ArrayList<String> get_subtasks() {
 		return _subtasks;
 	}
-	
+
 	public void set_subtasks(ArrayList<String> _subtasks) {
 		this._subtasks = _subtasks;
 	}
-	
+
 	public ArrayList<String> get_components() {
 		return _components;
 	}
-	
+
 	public void set_components(ArrayList<String> _components) {
 		this._components = _components;
 	}
-	
+
 	public String get_typeIssue() {
 		return _typeIssue;
 	}
-	
+
 	public void set_typeIssue(String _typeIssue) {
 		this._typeIssue = _typeIssue;
 	}
-	
+
 	public String get_statusIssue() {
 		return _statusIssue;
 	}
-	
+
 	public void set_statusIssue(String _statusIssue) {
 		this._statusIssue = _statusIssue;
 	}
-	
+
 	public String get_priorityIssue() {
 		return _priorityIssue;
 	}
-	
+
 	public void set_priorityIssue(String _priorityIssue) {
 		this._priorityIssue = _priorityIssue;
 	}
-	
+
 	public Date get_creationDate() {
 		return _creationDate;
 	}
-	
+
 	public void set_creationDate(Date _creationDate) {
 		this._creationDate = _creationDate;
 	}
-	
+
 	public Date get_modificationDate() {
 		return _modificationDate;
 	}
-	
+
 	public void set_modificationDate(Date _modificationDate) {
 		this._modificationDate = _modificationDate;
 	}
+
 	public String get_environment() {
 		return _environment;
 	}
-	
+
 	public void set_environment(String _environment) {
 		this._environment = _environment;
 	}
-	
+
 	public String get_description() {
 		return _description;
 	}
-	
+
 	public void set_description(String _description) {
 		this._description = _description;
 	}
-	
-	
 
 }
